1f4be6a367eca0bf3f746f88c0929f85e573f5c9
Zwe Mun Htun <zwemunhtun@ucsy.edu.mm>
2019-05-10 17:33:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ function shout(string) {
 }
 
 var uppercase = "HELLO!"
- 
+
 uppercase.toUpperCase() === uppercase // true
- 
+
 var lowercase = 'hello!'
- 
+
 lowercase.toLowerCase() === lowercase // true
- 
+
 var mixedCase = 'Hi there!'
- 
+
 mixedCase.toLowerCase() === mixedCase // false
- 
+
 mixedCase.toUpperCase() === mixedCase // false
